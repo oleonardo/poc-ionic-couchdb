ddab385055c8b3f35c270ed3b4e6b9df61f193d5
Leonardo de Oliveira <leonardodeoliveira@MBL-K1JQ0J614C.local>
2024-09-19 16:12:50 -0300

    feat: Initial commit

---
+++ sx-poc/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.poc.sx.app',
+  appName: 'sx-poc',
+  webDir: 'www'
+};
+
+export default config;
